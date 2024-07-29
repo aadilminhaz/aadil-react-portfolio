94a5157170c211f77aa5ec2ff0f53df5b63b52ce
Aadil Minhaz <aadilminhaz@Aadils-MacBook-Air.local>
2024-07-30 01:51:54 +0530

    Added upcoming blog section, added contact, ready to launch

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
                 <div className='footer-bottom-right'>
                     <p>Term of Services</p>
                     <p>Privacy Policy</p>
-                    <p>Connect with me</p>
+                    
                 </div>
             </div>
         </div>       
