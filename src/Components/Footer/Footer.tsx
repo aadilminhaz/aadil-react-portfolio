f314291be33fbb6cec9294bbf90335ee1999bafc
Aadil Minhaz <aadilminhaz@Aadils-MacBook-Air.local>
2024-07-30 12:27:22 +0530

    After launch responsive fix

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,19 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='footer'>
-            <div className='footer-top'>
-                <div className='footer-top-left'>
-                    <img src={footer_logo} alt=""/>
-                    <p>I am a Backend developer based in UK with 7+ years of experience</p>
-                </div>
-                <div className='footer-top-right'>
-                    <div className='footer-email-input'>
-                        
-                        <input type="email" placeholder="Enter your email"/>
-                    </div>    
-                    <div className='footer-subscribe'>Subscribe</div>
-                </div>
-            </div>
+            
             <hr/>
             <div className='footer-bottom'>
                 <p className='footer-bottom-left'>@ 2024 Aadil Minhaz. All right reserved</p>
